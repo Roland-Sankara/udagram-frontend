05b7bd902a3ff9671349c84609fda15582055269
Roland-Sankara <roland1sankara@gmail.com>
2022-10-01 14:17:04 +0300

    update apiHost to target reverseproxy external-ip

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a6973d7b73a544856946899a3cd8dd46-739580892.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
