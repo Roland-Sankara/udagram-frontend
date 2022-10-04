1807a752496a12723ca0c713eae92de4ef51faf9
Roland-Sankara <roland1sankara@gmail.com>
2022-10-04 17:56:32 +0300

    update apiHost url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://local:8080/api/v0'
+  apiHost: 'http://a681861b0cc504072bacc99be2d1e899-895319308.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
