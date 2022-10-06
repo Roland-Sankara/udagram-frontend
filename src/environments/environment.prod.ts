d0227df5752c79ac3653fe54ed751ba2d09b362c
Roland-Sankara <roland1sankara@gmail.com>
2022-10-06 15:19:20 +0300

    update apihost and travis config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://a681861b0cc504072bacc99be2d1e899-895319308.us-east-2.elb.amazonaws.com:8080/api/v0'
+  apiHost: 'http://a6a6e0eb623b94386af6f006d6d639e5-151200432.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
